a3bff63768b166b79269894873289aafe69581ad
Ignat <lookgoodmeat@gmail.com>
2026-04-09 08:12:17 +0000

    fix: biome import order

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createTelegramClient, disconnectClient, getClient } from "./client.js"
 import { resolveConfig } from "./config.js"
 import { decryptSession, isEncryptedSession } from "./crypto.js"
 
-import { sendTextReply, setupInboundHandler, type InboundMessage } from "./handler.js"
+import { type InboundMessage, sendTextReply, setupInboundHandler } from "./handler.js"
 
 interface ChannelPluginAPI {
 	config: Record<string, any>
